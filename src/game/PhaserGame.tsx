14e5b7dde09dd6b7cd712f55d9c9a4701fe58877
Adam Simpson <adam.simpson@mantiumai.com>
2024-04-25 18:39:47 -0700

    switching to react template

--- src/game/PhaserGame.tsx
+++ src/game/PhaserGame.tsx
@@ -17,6 +17,7 @@ export const PhaserGame = forwardRef<IRefPhaserGame, IProps>(
 
         useLayoutEffect(() => {
             if (game.current === null) {
+                console.log("about to start game 2", game);
                 game.current = StartGame("game-container");
 
                 if (typeof ref === "function") {
@@ -28,6 +29,7 @@ export const PhaserGame = forwardRef<IRefPhaserGame, IProps>(
 
             return () => {
                 if (game.current) {
+                    console.log("starting game 1");
                     game.current.destroy(true);
                     if (game.current !== null) {
                         game.current = null;
--- src/game/main.ts
+++ src/game/main.ts
@@ -1,15 +1,15 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import {Fight} from './scenes/Fight';
-import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
-import {Overworld} from "@/game/scenes/Overworld";
-import {Hud} from "@/game/scenes/Hud";
-import {Shop} from "@/game/scenes/Shop";
-import {NewGame} from "@/game/scenes/NewGame";
-import {BossFight} from "@/game/scenes/BossFight";
-import {GameWon} from "@/game/scenes/GameWon";
+import { Boot } from "./scenes/Boot";
+import { GameOver } from "./scenes/GameOver";
+import { Fight } from "./scenes/Fight";
+import { MainMenu } from "./scenes/MainMenu";
+import { AUTO, Game } from "phaser";
+import { Preloader } from "./scenes/Preloader";
+import { Overworld } from "@/game/scenes/Overworld";
+import { Hud } from "@/game/scenes/Hud";
+import { Shop } from "@/game/scenes/Shop";
+import { NewGame } from "@/game/scenes/NewGame";
+import { BossFight } from "@/game/scenes/BossFight";
+import { GameWon } from "@/game/scenes/GameWon";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -17,10 +17,10 @@ const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
     width: 1024,
     height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
+    parent: "game-container",
+    backgroundColor: "#028af8",
     physics: {
-        default: 'arcade',
+        default: "arcade",
         arcade: {
             debug: false,
         },
@@ -28,8 +28,11 @@ const config: Phaser.Types.Core.GameConfig = {
     render: {
         antialiasGL: false,
         pixelArt: true,
-    }, 
+    },
     autoFocus: true,
+    callbacks: {
+        preBoot: console.trace,
+    },
     scene: [
         Boot,
         Preloader,
@@ -42,11 +45,11 @@ const config: Phaser.Types.Core.GameConfig = {
         Hud,
         NewGame,
         MainMenu,
-    ]
+    ],
 };
 
 const StartGame = (parent: string) => {
     return new Game({ ...config, parent });
-}
+};
 
 export default StartGame;
