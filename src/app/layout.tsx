e63194cc12b661b41396c9de235a5259cdb18072
Adam Simpson <adam.simpson@mantiumai.com>
2024-05-09 10:43:57 -0700

    removing grid guesser

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Grid Guesser",
+    title: "Broom Sweeper",
     description: "Minesweeper Roguelike",
 };
 
